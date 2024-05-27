acd1e0de1f68093426216c98cefeb9a32cc34b5f
Adam <arbobeck@gmail.com>
2024-05-27 10:35:46 +0200

    update doom config

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages '(vue-mode))
  '(cua-mode t nil (cua-base))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(chatgpt-shell vue-mode))
  '(tool-bar-mode nil nil (tool-bar)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

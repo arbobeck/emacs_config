33c447ec610eb27baa5b08d933a25e1a5ca9de46
Adam <arbobeck@gmail.com>
2025-12-27 08:51:39 +0100

    update config.el for ubuntu vterm

--- config.el
+++ config.el
@@ -78,3 +78,5 @@
 ;; they are implemented.
 
 (set-frame-parameter (selected-frame) 'alpha '(90 . 90)) (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+
+(setq vterm-module-cmake-args '("-DUSE_SYSTEM_LIBVTERM=ON"))
